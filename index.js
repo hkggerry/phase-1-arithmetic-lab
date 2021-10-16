51802bb6e0f4cca7a1ad3d6099abee038a8646d9
gerry <hkggerry@gmail.com>
2021-10-15 17:34:47 -0700

    Completeled Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1*num2
+
+
+Number.isInteger(random)
+let random = 1
+
+const num3 = 16
+const num4 = 6
+
+let mod = num3%num4
+
+let max = Math.max(2,3,6,8,20)
